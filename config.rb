1694bf0863371234f573274cd0f6d05c46133bac
moroz <morozarturv@gmail.com>
2014-08-26 14:03:38 +0300

    fixes

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 
 
@@ -46,7 +46,7 @@ preferred_syntax = :sass
 # module Sass::Script::Functions
 #   def image_path(string)
 #     assert_type string, :String
-#     Sass::Script::String.new("../img/#{string.value}")
+#     Sass::Script::String.new("../images/#{string.value}")
 #   end
 #   alias_method :"image-path",:image_path 
 #   declare :"image-path", :args => [:string]
